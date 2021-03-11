d9dd9197ff95cd9f16a8804089991e64ad7c44c6
Ethan Rose <erose@apache.org>
2021-03-11 09:32:15 -0500

    Switch to using a single datanode for integration test
    Test still has issues after upgrade.

--- hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/hdds/upgrade/TestContainerSchemaVersions.java
+++ hadoop-ozone/integration-test/src/test/java/org/apache/hadoop/hdds/upgrade/TestContainerSchemaVersions.java
@@ -19,10 +19,14 @@ package org.apache.hadoop.hdds.upgrade;
 
 import org.apache.commons.codec.CharEncoding;
 import org.apache.commons.io.FileUtils;
+import org.apache.derby.impl.store.raw.data.ContainerOperation;
 import org.apache.hadoop.hdds.HddsConfigKeys;
 import org.apache.hadoop.hdds.client.ReplicationFactor;
 import org.apache.hadoop.hdds.client.ReplicationType;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
+import org.apache.hadoop.hdds.protocolPB.SCMSecurityProtocolClientSideTranslatorPB;
+import org.apache.hadoop.hdds.scm.cli.ContainerOperationClient;
+import org.apache.hadoop.hdds.scm.cli.ScmOption;
 import org.apache.hadoop.hdds.scm.pipeline.Pipeline;
 import org.apache.hadoop.hdds.scm.pipeline.PipelineManager;
 import org.apache.hadoop.ozone.HddsDatanodeService;
@@ -34,6 +38,9 @@ import org.apache.hadoop.ozone.client.io.OzoneInputStream;
 import org.apache.hadoop.ozone.client.io.OzoneOutputStream;
 import org.apache.hadoop.ozone.container.common.interfaces.Container;
 import org.apache.hadoop.ozone.om.OMConfigKeys;
+import org.apache.hadoop.ozone.om.ScmClient;
+import org.apache.hadoop.ozone.upgrade.UpgradeFinalizer;
+import org.apache.hadoop.test.GenericTestUtils;
 import org.apache.hadoop.test.LambdaTestUtils;
 import org.junit.After;
 import org.junit.Assert;
@@ -41,37 +48,36 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.apache.hadoop.hdds.HddsConfigKeys.HDDS_PIPELINE_REPORT_INTERVAL;
-import static org.apache.hadoop.hdds.protocol.proto.HddsProtos.ReplicationFactor.THREE;
+import static org.apache.hadoop.hdds.protocol.proto.HddsProtos.ReplicationFactor.ONE;
 import static org.apache.hadoop.hdds.protocol.proto.HddsProtos.ReplicationType.RATIS;
+import static org.apache.hadoop.hdds.protocol.proto.HddsProtos.ReplicationType.STAND_ALONE;
 import static org.apache.hadoop.hdds.scm.ScmConfigKeys.OZONE_DATANODE_PIPELINE_LIMIT;
 import static org.apache.hadoop.hdds.scm.pipeline.Pipeline.PipelineState.OPEN;
 
-/**
- * Tests that DataNode will throw an exception on creation when it reads in a
- * VERSION file indicating a metadata layout version larger than its
- * software layout version.
- */
 public class TestContainerSchemaVersions {
-  @Rule
-  public TemporaryFolder hddsMetadataDir = new TemporaryFolder();
-  public TemporaryFolder omMetadataDir = new TemporaryFolder();
-  public TemporaryFolder ozoneMetadataDir = new TemporaryFolder();
-
-  private static final int NUM_DATA_NODES = 3;
+  private static final int NUM_DATA_NODES = 1;
+  private static final Logger LOG =
+      LoggerFactory.getLogger(TestContainerSchemaVersions.class);
 
   private OzoneConfiguration conf;
   private MiniOzoneCluster cluster;
+  private ContainerOperationClient scmClient;
+  private String scmClientID;
 
   @Before
   public void setup() throws Exception {
@@ -79,9 +85,6 @@ public class TestContainerSchemaVersions {
     conf.setTimeDuration(HDDS_PIPELINE_REPORT_INTERVAL, 1000,
         TimeUnit.MILLISECONDS);
     conf.set(OZONE_DATANODE_PIPELINE_LIMIT, "1");
-//    conf.set(HddsConfigKeys.HDDS_METADATA_DIR_NAME, hddsMetadataDir.toString());
-//    conf.set(OMConfigKeys.OZONE_OM_DB_DIRS, omMetadataDir.toString());
-    conf.set(OzoneConfigKeys.OZONE_METADATA_DIRS, ozoneMetadataDir.toString());
     cluster = null;
   }
 
@@ -126,7 +129,12 @@ public class TestContainerSchemaVersions {
     // When using new MLV, the new schema version field should be
     // persisted to container files for new container only.
     // Old container files should not be updated.
-    createNewCluster(HDDSLayoutFeature.DATANODE_SCHEMA_V2);
+//    createNewCluster(HDDSLayoutFeature.DATANODE_SCHEMA_V2);
+    // TODO: Move to init.
+    scmClient = new ContainerOperationClient(conf);
+    scmClientID = "baz";
+    finalizeUpgrade();
+
 
     // When using old MLV, the new schema version field should not be
     // persisted to the container files.
@@ -186,8 +194,8 @@ public class TestContainerSchemaVersions {
     store.getVolume(content).createBucket(content);
     OzoneOutputStream stream =
         store.getVolume(content).getBucket(content)
-        .createKey(content, 10, ReplicationType.RATIS,
-            ReplicationFactor.THREE, new HashMap<>());
+        .createKey(content, 10, ReplicationType.STAND_ALONE,
+            ReplicationFactor.ONE, new HashMap<>());
 
     stream.write(content.getBytes(UTF_8));
     stream.close();
@@ -214,8 +222,7 @@ public class TestContainerSchemaVersions {
     shutdown();
     cluster = MiniOzoneCluster.newBuilder(conf)
         .setNumDatanodes(NUM_DATA_NODES)
-        // allow only one FACTOR THREE pipeline.
-        .setTotalPipelineNumLimit(NUM_DATA_NODES + 1)
+        .setTotalPipelineNumLimit(NUM_DATA_NODES)
         .setHbInterval(1000)
         .setHbProcessorInterval(1000)
         .setScmLayoutVersion(layoutVersion.layoutVersion())
@@ -223,12 +230,28 @@ public class TestContainerSchemaVersions {
         .build();
     cluster.waitForClusterToBeReady();
 
-    // Wait for the one ratis factor 3 pipeline.
-    PipelineManager scmPipelineManager = cluster.getStorageContainerManager().getPipelineManager();
-    LambdaTestUtils.await(10000, 2000, () -> {
-      List<Pipeline> pipelines =
-          scmPipelineManager.getPipelines(RATIS, THREE, OPEN);
-      return pipelines.size() == 1;
-    });
+//    PipelineManager scmPipelineManager = cluster.getStorageContainerManager().getPipelineManager();
+//    LambdaTestUtils.await(10000, 2000, () -> {
+//      List<Pipeline> pipelines =
+//          scmPipelineManager.getPipelines(STAND_ALONE, ONE, OPEN);
+//      return pipelines.size() == NUM_DATA_NODES;
+//    });
+  }
+
+  private void finalizeUpgrade()
+      throws TimeoutException, InterruptedException, IOException {
+    scmClient.finalizeScmUpgrade(scmClientID);
+
+    GenericTestUtils.waitFor(() -> {
+      try {
+        UpgradeFinalizer.StatusAndMessages statusAndMessages =
+            scmClient.queryUpgradeFinalizationProgress(scmClientID, false);
+        LOG.info("Finalization Messages : " + statusAndMessages.msgs());
+        return statusAndMessages.status().equals(UpgradeFinalizer.Status.FINALIZATION_DONE);
+      } catch (IOException e) {
+        Assert.fail(e.getMessage());
+      }
+      return false;
+    }, 2000, 20000);
   }
 }
