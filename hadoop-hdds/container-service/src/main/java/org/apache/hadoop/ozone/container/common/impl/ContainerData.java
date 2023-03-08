5479c3e0e0d4cc6ab28f6d9575b9ac8494d7ec81
Ethan Rose <erose@apache.org>
2023-03-08 13:54:49 -0800

    Initial addition of isEmpty flag to protos and wrappers for SCM and DN

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/impl/ContainerData.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/common/impl/ContainerData.java
@@ -101,6 +101,8 @@ public abstract class ContainerData {
 
   private String checksum;
 
+  private boolean isEmpty;
+
   /** Timestamp of last data scan (milliseconds since Unix Epoch).
    * {@code null} if not yet scanned (or timestamp not recorded,
    * eg. in prior versions). */
@@ -154,6 +156,7 @@ public abstract class ContainerData {
     this.maxSize = size;
     this.originPipelineId = originPipelineId;
     this.originNodeId = originNodeId;
+    this.isEmpty = false;
     setChecksumTo0ByteArray();
   }
 
@@ -535,6 +538,14 @@ public abstract class ContainerData {
    */
   public long getBlockCount() {
     return this.blockCount.get();
+  }
+
+  public boolean isEmpty() {
+    return isEmpty;
+  }
+
+  public void setEmpty(boolean isEmpty) {
+    this.isEmpty = isEmpty;
   }
 
   /**
