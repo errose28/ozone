afc4a8d1645adfddd152a51e57068dcb75f7ad7b
Ethan Rose <erose@apache.org>
2026-05-15 14:07:38 -0400

    Add new SCM version manager

---
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/server/upgrade/ScmVersionManager.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.hadoop.hdds.scm.server.upgrade;
+
+import com.google.common.annotations.VisibleForTesting;
+import java.io.IOException;
+import java.util.Map;
+import org.apache.hadoop.hdds.ComponentVersion;
+import org.apache.hadoop.hdds.scm.server.SCMStorageConfig;
+import org.apache.hadoop.hdds.upgrade.HDDSVersionManager;
+import org.apache.hadoop.hdds.upgrade.ScmUpgradeAction;
+import org.apache.hadoop.hdds.upgrade.ScmUpgradeActionProvider;
+import org.apache.hadoop.ozone.upgrade.ComponentUpgradeActionProvider;
+import org.apache.hadoop.ozone.upgrade.UpgradeException;
+
+/**
+ * SCM-specific version manager that wires upgrade actions internally.
+ */
+public class ScmVersionManager extends HDDSVersionManager {
+
+  private final Map<ComponentVersion, ScmUpgradeAction> upgradeActions;
+  private final SCMUpgradeFinalizationContext upgradeActionArg;
+
+  public ScmVersionManager(SCMStorageConfig storage,
+      SCMUpgradeFinalizationContext upgradeActionArg) throws IOException {
+    this(storage, upgradeActionArg, new ScmUpgradeActionProvider());
+  }
+
+  @VisibleForTesting
+  public ScmVersionManager(SCMStorageConfig storage,
+      SCMUpgradeFinalizationContext upgradeActionArg,
+      ComponentUpgradeActionProvider<ScmUpgradeAction> upgradeActionProvider)
+      throws IOException {
+    super(storage);
+    this.upgradeActionArg = upgradeActionArg;
+    upgradeActions = upgradeActionProvider.load();
+  }
+
+  @VisibleForTesting
+  public Map<ComponentVersion, ScmUpgradeAction> getUpgradeActionsForTesting() {
+    return upgradeActions;
+  }
+
+  @Override
+  protected void runUpgradeAction(ComponentVersion version) throws UpgradeException {
+    ScmUpgradeAction action = upgradeActions.get(version);
+    if (action == null) {
+      return;
+    }
+    try {
+      action.execute(upgradeActionArg);
+    } catch (Exception e) {
+      logAndThrow(e, "SCM upgrade action for version " + version + " failed.",
+          UpgradeException.ResultCodes.FINALIZE_UPGRADE_ACTION_FAILED);
+    }
+  }
+}
